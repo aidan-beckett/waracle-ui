9bb7fd0a1c1c470475289009359435d1958272e9
Aidan Beckett <aidan.beckett@synextra.co.uk>
2022-02-09 11:00:31 +0000

    cra

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../public/css/App.css';
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="app">
+      <Router>
+        
+      </Router>
+    </div>
+  );
+}
+
+export default App;
